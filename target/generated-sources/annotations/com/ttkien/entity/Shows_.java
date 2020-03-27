cfa72b86ca0cc155976ba2e2fbee2491b8db6ec8
Kien Tran <n01260692@humbermail.ca>
2020-03-27 18:47:40 -0400

    Fix hanler xml location

--- target/generated-sources/annotations/com/ttkien/entity/Shows_.java
+++ target/generated-sources/annotations/com/ttkien/entity/Shows_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-03-27T17:55:42")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-03-27T18:40:44")
 @StaticMetamodel(Shows.class)
 public class Shows_ { 
 
